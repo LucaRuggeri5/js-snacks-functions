3f4708fa2d0d9684d9531ca65367d67c0910c222
LucaRuggeri5 <luca.ruggeri5@gmail.com>
2025-09-03 17:12:17 +0200

    finalizzato esewrcizio 3

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -4,10 +4,18 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 
 // Dichiara la funzione qui.
-
+function inizialeA(array, letter) {
+  const result = [];
+  for (let i = 0; i < array.length; i++) {
+    if (array[i][0] === letter) {
+      result.push(array[i]);
+    }
+  }
+  return result;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
+const filteredNames = inizialeA(names, "A");
+console.log(filteredNames);
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
